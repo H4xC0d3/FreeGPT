d2350e8b6fb97f6197460e51da1debed264aacb3
H4xC0d3 <pyTroy@proton.me>
2024-09-11 18:04:56 +0300

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 client = OpenAI(
     base_url="http://gpt.catway.org/v1",
-    api_key="."
+    api_key="sk-r4khWXD9FY4NkC7GO4jWfTSuB3G3cdS0yyBXKnO9F7Pjm4YC"
 )
 
 chat_completion = client.chat.completions.create(
